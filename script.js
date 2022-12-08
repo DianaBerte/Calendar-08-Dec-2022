23a2e17b662938dba20c3ec99d8a8feedc557cce
DianaBerte <dianaberte.go@gmail.com>
2022-12-08 12:58:41 +0100

    committing

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-function onDayClick(event) { //this is an event listener, so it'll receive as parameter an event 
+//GLOBAL VARIABLES - the memory of our calendar:
+
+/*basically, it'd be a LIST OF APPOINTMENTS - OBJECTS, bc a calendar can have a list of appointments for each day.
+
+
+let appointmentsForDay1 = [
+    {time: "9:00", title: "Epicode lecture"},
+    {time: "12:30", title: "lunch"},
+];*/
+
+let appointments = [
+    [{time: "9:00", title: "Epicode lecture"}, {time: "12:30", title: "lunch"}], //list of appointments for day 1
+    [], //day 2
+    [], //day 3
+    ///... and so on, for 30 days
+]
+
+function visualizeAppointments (event) {
+    let selectedDay = event.target.innerText;
+    selectedDayNumber = parseInt(selectDay);
+    let index = selectedDayNumber - 1;
+    let appointmentsForSelectedDay = appointments[index]
+
+    for (let appointment of appointmentsForSelectedDay) {
+        let appointmentLi = document.createElement('li');
+
+        appointmentLi.innerText = `${}`
+
+        document.getElementById("appointments-list").appendChild(appointmentLi)
+
+    }
+}
+
+function selectDay(event) { //this is an event listener, so it'll receive as parameter an event 
     //we clicked a day! how do we know which one?
     
     let previouslySelectedDay = document.querySelector('.selected');
@@ -14,6 +47,11 @@ function onDayClick(event) { //this is an event listener, so it'll receive as pa
     
     
     clickedDayNode.classList.add('selected') //now we're selecting the days and they stay selected
+}
+
+function onDayClick(event) {
+    selectDay(event)
+    visualizeAppointments(event);
 }
 
 

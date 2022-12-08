6efcf659a94de0fb993ccc9c7e133b6c0f12ef3d
DianaBerte <dianaberte.go@gmail.com>
2022-12-08 10:35:33 +0100

    display days

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert('Hello')
+function onLoadActions() {
+    //TODO Highlight Extension
+    
+    for (let day = 1; day <= 30; day++) {
+        let dayNode = document.createElement('div') //step 1: create the element div
+        
+        dayNode.classList.add('day') //step 2: this is for the style
+        dayNode.innerText = day //still step 2
+
+        document.getElementById('days-container').appendChild(dayNode) // step 3
+
+
+    }
+}
+
+window.onload = onLoadActions
